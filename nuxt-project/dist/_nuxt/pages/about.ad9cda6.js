6af84fb79d6775f436be22f659fe7d2d83ff9c24
timofey-TK <67265641+timofey-TK@users.noreply.github.com>
2020-08-21 14:52:27 +0300

    dist

---
+++ nuxt-project/dist/_nuxt/pages/about.ad9cda6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{237:function(t,e,n){"use strict";n.r(e);var l={},c=n(20),component=Object(c.a)(l,(function(){var t=this.$createElement;return(this._self._c||t)("h1",[this._v("About page")])}),[],!1,null,null,null);e.default=component.exports}}]);
