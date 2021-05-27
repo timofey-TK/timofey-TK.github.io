aaa0c48e8db8b15812a80009fe1a51dc8679b471
TIMOFEY <67265641+timofey-TK@users.noreply.github.com>
2021-05-27 15:12:56 +0300

    reclama preview

---
+++ reclamaagent/script.js
@@ -0,0 +1,51 @@
+// Get the navbar
+const sections = document.querySelectorAll(".scroll")
+console.log(sections)
+
+window.onscroll = function () {
+  sections.forEach((el, i) => {
+    var top = el.offsetTop - 100;
+    var bottom = top + el.offsetHeight;
+    var scroll = window.pageYOffset
+    var id = el.getAttribute("id")
+    if (scroll > top && scroll < bottom) {
+      document.querySelector(".nav__link a.active").classList.remove('active')
+      var href = "#" + id
+      document.querySelector('a[href = "' + href + '"').classList.add('active')
+    }
+  })
+};
+
+const smoothLinks = document.querySelectorAll('.nav__link a');
+for (let smoothLink of smoothLinks) {
+  smoothLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = smoothLink.getAttribute('href');
+
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+};
+
+// Меню для мобилок
+const links = document.querySelectorAll('.nav__link a')
+const hamburger = document.querySelector(".hamburger")
+const navBar = document.querySelector(".nav-bar")
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("is-active");
+  navBar.classList.toggle("opened");
+});
+links.forEach((el) => {
+  el.addEventListener("click", function () {
+    hamburger.classList.toggle("is-active");
+    navBar.classList.toggle("opened");
+  })
+})
+
+
+
+
+
+
