64716ed5c35bd3c42cd570a8bbc108c73c2975bf
TIMOFEY <67265641+timofey-TK@users.noreply.github.com>
2021-10-26 17:33:43 +0300

    ss

--- consortium/scripts/script.js
+++ consortium/scripts/script.js
@@ -6,7 +6,32 @@ document.addEventListener('DOMContentLoaded', function () {
             items: 1,
             nav: true,
             dots: false,
-            navText: ['<div class="arrow prev-arrow"><img src="media/icons/slider-arrow.svg" alt="arrow"/></div>', '<div class="arrow next-arrow"><img src="media/icons/slider-arrow.svg" alt="arrow"/></div>']
+            navText: ['<div class="arrow prev-arrow"><img src="media/icons/slider-arrow.svg" alt="arrow"/></div>', '<div class="arrow next-arrow"><img src="media/icons/slider-arrow.svg" alt="arrow"/></div>'],
+            responsive: {
+                // breakpoint from 0 up
+                0: {
+                    stagePadding: 0,
+                    nav: false
+                },
+                290: {
+                    stagePadding: 30,
+                    nav: false
+                },
+                510: {
+                    stagePadding: 50
+                },
+                595: {
+                    stagePadding: 80
+                },
+                640: {
+                    stagePadding: 100
+                },
+                // breakpoint from 768 up
+                768: {
+                    stagePadding: 0,
+                    nav: true
+                }
+            }
         }
     );
 }); // end ready
