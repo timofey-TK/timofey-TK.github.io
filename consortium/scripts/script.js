b2bf2268c72adc4cdeba588d496dab4a7cfb40b6
TIMOFEY <67265641+timofey-TK@users.noreply.github.com>
2021-10-31 11:53:08 +0300

    vh

--- consortium/scripts/script.js
+++ consortium/scripts/script.js
@@ -106,5 +106,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     );
 
-
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
 }); // end ready
