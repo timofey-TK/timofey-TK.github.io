386249ee0233a9997798c096b7a1d40e3b53d927
TIMOFEY <67265641+timofey-TK@users.noreply.github.com>
2022-04-11 19:33:39 +0300

    updated

--- consortium/scripts/landing-calc.js
+++ consortium/scripts/landing-calc.js
@@ -1,56 +1,53 @@
-$(function () {
-    function filter(x) {
-        return x.toString().replace(/[^\.\-0-9]/gim, '');
+function validate(evt) {
+    var theEvent = evt || window.event;
+    var key = theEvent.keyCode || theEvent.which;
+    key = String.fromCharCode(key);
+    var regex = /[0-9]|\./;
+    if (!regex.test(key)) {
+        theEvent.returnValue = false;
+        if (theEvent.preventDefault) theEvent.preventDefault();
+    }
+}
+
+function calc() {
+    messages = document.querySelector("#messages")
+    ctr = document.querySelector(".ctr")
+    convers = document.querySelector(".convers")
+    msprice = document.querySelector(".price-click")
+    isBD = document.querySelector("#checkbox1")
+    isAD = document.querySelector("#checkbox2")
+    request = document.querySelector(".request")
+    requestPrice = document.querySelector(".request-price")
+    // isAD.checked
+    if (isBD.checked && !isAD.checked) {
+        msprice.value = 3
+    }
+    if (isAD.checked && !isBD.checked) {
+        msprice.value = 2.5
+    }
+    if (isBD.checked && isAD.checked) {
+        msprice.value = 3.5
     }
 
-
-    function numberWithCommas(x) {
-        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-    }
-
-    //$('.onlyDigits').inputmask({alias: 'numeric',
-    //allowMinus: false,
-    // digits: 2,
-
-    // max: 999999999.99});
-
-    $('.js-calc input').on('change', function () {
-        $(this).val($(this).val().toString().replace(/\,/g, '.'));
-
-        $(this).val(filter($(this).val()));
-        $(this).val(numberWithCommas($(this).val()));
+    request.innerHTML = Math.round(Number("0." + String(ctr.value)) * Number("0." + String(convers.value)) * messages.value)
+    requestPrice.innerHTML = Math.round((messages.value * msprice.value) / request.innerHTML)
+}
 
 
+let syncRange = document.querySelector(".messages_range"),
+    syncMessages = document.querySelector(".messages")
 
+let calcInputs = document.querySelectorAll(".js-calc input").forEach(i => {
+    i.oninput = function () {
+        if (i.classList.contains("messages")) {
+            syncRange.value = syncMessages.value / 1000
+            console.log("12344");
+        }
+        if (i.classList.contains("messages_range")) {
+            syncMessages.value = syncRange.value * 1000
+            console.log("12344");
+        }
+        calc()
 
-
-
-        var rekB = parseFloat($('.rek-b').val().replace(/\s/g, '')); //рекламный бюджет
-        var rekShow = $('.rek-show'); //показы рекламы
-        var transit = $('.transit'); //переходы на сайт
-        var priceClick = parseFloat($('.price-click').val().replace(/\s/g, '')); //цена клика
-        var ctr = parseFloat($('.ctr').val().replace(/\s/g, '')); //CTR
-        var convers = parseFloat($('.convers').val().replace(/\s/g, '')); //конверсия сайта
-        var request = $('.request'); //колчество заявок
-        var requestPrice = $('.request-price'); //стоимость заявки
-
-
-
-        var transitNum = Math.trunc(rekB / priceClick);
-        transit.text(transitNum.toLocaleString('ru'));
-
-        var rekShowNum = Math.trunc(transitNum * 100 / ctr);
-        rekShow.text(rekShowNum.toLocaleString('ru'));
-
-        var requestNum = Math.trunc(transitNum * convers / 100);
-        request.text(requestNum.toLocaleString('ru'));
-
-        var requestPriceNum = rekB / requestNum;
-        requestPriceNum = requestPriceNum.toFixed(2)
-        requestPrice.text(requestPriceNum.toLocaleString('ru'));
-    });
-
-
-
-    $('.js-calc input').trigger('change');
-});
+    };
+})
