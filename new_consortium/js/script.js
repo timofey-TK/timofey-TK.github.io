e5ff753556ccf9fb212ce43a7d4ef17abc955366
TIMOFEY <67265641+timofey-TK@users.noreply.github.com>
2022-12-14 21:20:07 +0300

    a

--- new_consortium/js/script.js
+++ new_consortium/js/script.js
@@ -16,10 +16,58 @@ const convertImages = (query) => {
             .catch(error => console.error(error))
     });
 }
+class ItcTabs {
+    constructor(target, config) {
+        const defaultConfig = {};
+        this._config = Object.assign(defaultConfig, config);
+        this._elTabs = typeof target === 'string' ? document.querySelector(target) : target;
+        this._elButtons = this._elTabs.querySelectorAll('.tabs__btn');
+        this._elPanes = this._elTabs.querySelectorAll('.tabs__pane');
+        this._eventShow = new Event('tab.itc.change');
+        this._init();
+        this._events();
+    }
+    _init() {
+        this._elTabs.setAttribute('role', 'tablist');
+        this._elButtons.forEach((el, index) => {
+            el.dataset.index = index;
+            el.setAttribute('role', 'tab');
+
+        });
+    }
+    show(elLinkTarget) {
+        const elPaneTarget = this._elPanes[elLinkTarget.dataset.index];
+        const elLinkActive = this._elTabs.querySelector('.tabs__btn_active');
+        const elPaneShow = this._elTabs.querySelector('.tabs__pane_show');
+        if (elLinkTarget === elLinkActive) {
+            return;
+        }
+        elLinkActive ? elLinkActive.classList.remove('tabs__btn_active') : null;
+        elPaneShow ? elPaneShow.classList.remove('tabs__pane_show') : null;
+        elLinkTarget.classList.add('tabs__btn_active');
+        elPaneTarget.classList.add('tabs__pane_show');
+        this._elTabs.dispatchEvent(this._eventShow);
+        elLinkTarget.focus();
+    }
+    showByIndex(index) {
+        const elLinkTarget = this._elButtons[index];
+        elLinkTarget ? this.show(elLinkTarget) : null;
+    };
+    _events() {
+        this._elTabs.addEventListener('click', (e) => {
+            const target = e.target.closest('.tabs__btn');
+            if (target) {
+                e.preventDefault();
+                this.show(target);
+            }
+        });
+    }
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     convertImages(".svg")
-
+    new ItcTabs('#cases-tabs');
+    new ItcTabs('#services-tabs');
     var burger = document.querySelector(".burger"),
         menu = document.querySelector("nav"),
         cross = document.querySelector(".cross")
@@ -41,5 +89,7 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         });
     };
+
+
 
 });
